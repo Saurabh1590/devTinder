35ed0cf3d1a7bcc63ebbab032815a465b2aa37bb
Saurabh Sahani <saurabhsahani2710@gmail.com>
2024-12-08 04:50:02 +0530

    exploring get, put,  delete, patch

--- src/app.js
+++ src/app.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.use("/jisoo", (req, res) => {
-  res.send("Hello Jisoo!");
+app.get("/user", (req, res) => {
+  res.send({firstname: "Saurabh", lastname: "Sahani"})
 });
 
-app.use("/lisa", (req, res) => {
-  res.send("Hello lisa!");
+app.post("/user", (req, res) => {
+  res.send("data saved to database sucessfully!");
 });
 
-app.use("/jennie", (req, res) => {
-    res.send("Hello Jennie!");
-  });
+app.delete("/user", (req, res) => {
+  res.send("Deleted the data sucessfully!");
+});
 
-  app.use((req, res) => {
-    res.send("Hello Saurabh!");
-  });
-  
-  
+app.patch("/user", (req, res) => {
+  res.send("Patched the data sucessfully!");
+});
+
+
+app.use("/hello", (req, res) => {
+  res.send("Hello Saurabh 2");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello Saurabh");
+});
 
 app.listen(7777, () => {
   console.log("Server is listining at port 7777");
