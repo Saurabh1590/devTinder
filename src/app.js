f7603e786200afb0de469fd0ae70839588bc70f0
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-04-03 04:00:06 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test",(req,res) => {
-    res.send("Hello from the server");
+app.get("/user",(req, res) => {
+    res.send({firstName: "Saurabh", lastName: "Sahani"})
 })
+
+app.post("/user",(req, res) => {
+    res.send("Data succesfully saved to database!");
+})
+
+app.patch("/user",(req, res) => {
+    res.send("patched succesfully!");
+})
+
+app.delete("/user",(req, res) => {
+    res.send("deleted succesfully!");
+})
+
+app.use("/test",(req,res) => {
+    res.send("hello from the server!");
+})
+
 
 app.listen((7777), () => {
     console.log("Server is successfully listening on port 7000...")
