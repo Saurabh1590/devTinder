42e68ab492814c8205d9b524b0ec151f2d0b7ea7
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-04-17 23:56:00 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,28 +1,32 @@
-const express = require('express');
-
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.get("/user",(req, res) => {
-    res.send({firstName: "Saurabh", lastName: "Sahani"})
-})
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Saurabh",
+    lastName: "Sahani",
+    emailId: "saurabh@1234.com",
+    password: "25r2wftsds",
+    age: 23,
+  });
 
-app.post("/user",(req, res) => {
-    res.send("Data succesfully saved to database!");
-})
-
-app.patch("/user",(req, res) => {
-    res.send("patched succesfully!");
-})
-
-app.delete("/user",(req, res) => {
-    res.send("deleted succesfully!");
-})
-
-app.use("/test",(req,res) => {
-    res.send("hello from the server!");
-})
-
-
-app.listen((7777), () => {
-    console.log("Server is successfully listening on port 7000...")
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error Saving the user:" + err.message);
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("database connection established");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("database cannot be connected");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://devTinder:8CwgLg0aLYYeCw4r@devtinder.glagl.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectDB;
