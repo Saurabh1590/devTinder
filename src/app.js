730746c62bf54148bfd2c724660fdc5dd6872519
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-04-30 04:16:01 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -3,20 +3,80 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Luffy",
-    lastName: "Sahani",
-    emailId: "luffy@1234.com",
-    password: "25r2wftsd",
-    age: 23,
-  });
+app.use(express.json());
 
+// get user by id
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findById(req.body.id);
+    if (!user) {
+      res.send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    throw res.status(400).send("Something went wrong");
+  }
+});
+
+// find by email
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    if (users.length === 0) {
+      res.send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    throw res.status(400).send("Something went wrong");
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const user = await new User(req.body);
   try {
     await user.save();
     res.send("User Added Successfully");
   } catch (err) {
     res.status(400).send("Error Saving the user:" + err.message);
+  }
+});
+
+// delete a user from the database
+app.delete("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.id);
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//find by id and update
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User updated succesfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:"+ err.message);
+  }
+});
+
+//find by emaiid and update
+app.patch("/user", async (req, res) => {
+  const emailId = req.body.emailId;
+  const data = req.body;
+  try {
+    await User.findOneAndUpdate({ emailId }, data, { runValidators: true });
+    res.send("User updated succesfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,59 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      minLength: 2,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      minLength: 2,
+      maxLength: 20,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 20,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid!");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/026/966/960/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg",
+      maxLength: 100,
+    },
+    about: {
+      type: String,
+      default: "This is default value",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
