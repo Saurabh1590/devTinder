0a48e3927fea7e446df07cc29092b5cba4d88435
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-08-04 10:09:33 +0530

    minor fix

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    process.env.DATABASE_URL
+    "mongodb+srv://devTinder:8CwgLg0aLYYeCw4r@devtinder.glagl.mongodb.net/devTinder"
 )};
 
 module.exports = connectDB;
