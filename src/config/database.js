fb3172929881e418296c09ada6018e7bf739128f
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-11-23 02:58:58 +0530

    rename variable

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URL);
 };
 
 module.exports = connectDB;
