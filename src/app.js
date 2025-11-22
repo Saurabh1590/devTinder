3c664236f33f0bf0fd1c09f1f7ba123962165934
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-11-23 03:36:47 +0530

    fix cors origin

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ require("./utils/cronJob");
 
 app.use(
   cors({
-     origin: "https://shift-swap-frontend.onrender.com",
+    origin: "https://dev-tinder-web-phi.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   })
