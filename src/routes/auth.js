f1050ba1c74074fedf069290264d655ac5304f45
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-11-23 04:04:22 +0530

    fix: update CORS and cookie settings for Vercel + Render deployment

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,7 +10,8 @@ authRouter.post("/signup", async (req, res) => {
     // Validation of data
     validateSignUpData(req);
 
-    const { firstName, lastName, emailId, password, skills, about, gender } = req.body;
+    const { firstName, lastName, emailId, password, skills, about, gender } =
+      req.body;
     // Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
 
@@ -26,11 +27,15 @@ authRouter.post("/signup", async (req, res) => {
 
     const savedUser = await user.save();
     const token = await savedUser.getJWT();
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 36000000),
-      });
-      
-    res.json({message:"User Added Successfully", data: savedUser});
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      maxAge: 8 * 60 * 60 * 1000,
+    });
+
+    res.json({ message: "User Added Successfully", data: savedUser });
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
   }
@@ -49,7 +54,11 @@ authRouter.post("/login", async (req, res) => {
     if (isPasswordValid) {
       const token = await user.getJWT();
       res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 36000000),
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: "/",
+        maxAge: 8 * 60 * 60 * 1000,
       });
       res.send(user);
     } else {
@@ -61,8 +70,12 @@ authRouter.post("/login", async (req, res) => {
 });
 
 authRouter.post("/logout", async (req, res) => {
-  res.cookie("token", null, {
-    expires: new Date(Date.now()),
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+    expires: new Date(0),
   });
   res.send("User logout successfully");
 });
